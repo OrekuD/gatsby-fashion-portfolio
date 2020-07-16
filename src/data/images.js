e9e8df8575e0611e7b293039751eaa49bd40e4ac
unknown <opokudavid446@gmail.com>
2020-07-16 15:35:34 +0100

    Add new images

--- src/data/images.js
+++ src/data/images.js
@@ -1,19 +1,12 @@
-import Img1 from "../images/1.jpg"
-import Img2 from "../images/2.png"
-import Img3 from "../images/3.jpg"
-import Img4 from "../images/4.jpg"
-import Img5 from "../images/5.jpg"
-import Img6 from "../images/6.jpg"
-import Img7 from "../images/7.jpg"
-import Img8 from "../images/8.jpg"
-
 export const images = [
-  { id: "1", image: Img1 },
-  { id: "2", image: Img2 },
-  { id: "3", image: Img3 },
-  { id: "4", image: Img4 },
-  { id: "5", image: Img5 },
-  { id: "6", image: Img6 },
-  { id: "7", image: Img7 },
-  { id: "8", image: Img8 },
+  { id: "1", image: require("../images/1.jpg") },
+  { id: "2", image: require("../images/2.png") },
+  { id: "3", image: require("../images/3.jpg") },
+  { id: "5", image: require("../images/5.jpg") },
+  { id: "6", image: require("../images/6.jpg") },
+  { id: "7", image: require("../images/7.jpg") },
+  { id: "8", image: require("../images/8.jpg") },
+  { id: "9", image: require("../images/19.jpg") },
+  { id: "10", image: require("../images/15.jpg") },
+  { id: "11", image: require("../images/17.jpg") },
 ]
